01ddaed9c5a1aea1ccf1f2ce9ca3c099a428ad12
lgactna <lgonzalesna@gmail.com>
2024-04-12 16:53:11 -0700

    Fix dependency

--- backend/migrations/0001_initial.py
+++ backend/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("django_celery_results", "0012_taskresult_task_creator"),
+        ("django_celery_results", "0011_taskresult_periodic_task_name"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
